67dbca6471d7352ae5a04eb306260da991329653
anonuser3256 <>
2022-02-04 18:29:34 +0000

    Adding item selection area to wireframe

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 import '../style/App.css';
+import Item from "./Item";
+import Cart from "./Cart";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <div className="items">
+    const [totalCost, setTotalCost] = React.useState(0);
+    const [itemsInCart, setItemsInCart] = React.useState([]);
+
+    function updateCartItems(itemName, itemPrice) {
+        if (!itemsInCart.includes(itemName)) {
+            setItemsInCart(prevState => ([...prevState, itemName]));
+            setTotalCost(totalCost + parseFloat(itemPrice));
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="items">
+                <Item name="1" price="10" updateCartItems={updateCartItems} />
+                <Item name="2" price="17.50" updateCartItems={updateCartItems} />
+                <Item name="3" price="22" updateCartItems={updateCartItems} />
+                <Item name="4" price="35" updateCartItems={updateCartItems} />
+            </div>
+            <div className="cart-checkout">
+                <Cart />
+            </div>
 
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import '../style/Cart.css';
+import React from "react";
+
+function Cart(props) {
+
+    return (
+        <div className="cart-area">
+
+        </div>
+    );
+}
+
+export default Cart;
